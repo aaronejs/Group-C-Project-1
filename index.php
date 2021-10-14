ae25134bd704c1db700e36f4a7263ad03bea68b0
Aaron Eljas <aaron.eljas@student.nhlstenden.com>
2021-10-14 21:17:24 +0200

    Index.html to index.php
    Changed index page from html to php, so we can echo the events.
    
    Made events have a scrollbar when the content doesnt fit in the event container

--- index.php
+++ index.php
@@ -4,71 +4,56 @@
   <head>
     <meta charset="utf-8">
     <link href="Styles.css" type="text/css"
- 			rel="stylesheet"/>
-    <title></title>
+ 			rel="stylesheet" />
+    <title>NHLStenden Dance course</title>
   </head>
   <body>
-    <form action="index.php" method="post">
-        <label for="fname">Full name *</label><br>
-        <input type="text" id="fname" name="fname" placeholder="First">
-        <input type="text" id="lname" name="lname" placeholder="Last"><br>
-        <label for="email">E-mail *</label><br>
-        <input type="text" id="email" name="email" placeholder="E-mail"><br>
-        <label for="select">Contact type *</label><br>
-        <select class="ctype" name="formType" id="ctype">
-            <option value="Question" selected>Question</option>
-            <option value="Suggestion">Feedback</option>
-            <option value="Complaint">Complaint</option>
-        </select><br>
-        <label for="message">Message *</label><br>
-        <input type="textarea" style="resize:none" name="message" placeholder="Your message...">
-        <input type="submit" name="submit" value="Send">
-        <input type="reset" value="Reset form">
-    </form>
-    <?php
-        if(isset($_POST['submit'])){
-            if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['message'])){
-                $fname=$_POST['fname'];
-                $lname=$_POST['lname'];
-                $email=$_POST['email'];
-                $formType=$_POST['formType'];
-                echo "The ".$formType . " has been submitted.";
-                $message=$_POST['message'];
-                if ($formType!='Complaint'){
-                    if((!is_dir($formType.'/'))){
-                         // direction doesn't exist, make it
-                         mkdir($formType.'/');
-                        }
-                    if(file_exists($formType.'/'.$formType. '_' . $fname . '_' . $lname . '.txt')){
-                        //it's so the ''database'' can store put to 2 same entries from one person.
-                         file_put_contents($formType.'/'.$formType. '_' . $fname . '_' . $lname . '1.txt',
-                                        $formType.' from '.$fname." ".$lname.
-                                        "\n------------------------------\nDate: "
-                                        .date("Y/m/d").
-                                        "\n------------------------------\nEmail: "
-                                        .$email.
-                                        "\n------------------------------\n"
-                                        .$message.
-                                        "\n------------------------------\n\n");
-                    }else{
-                        //example, put Question_Evald_Narkevicius.txt to a Question folder.
-                        file_put_contents($formType.'/'.$formType. '_' . $fname . '_' . $lname . '.txt',
-                                        $formType.' from '.$fname." ".$lname.
-                                        "\n------------------------------\nDate: "
-                                        .date("Y/m/d").
-                                        "\n------------------------------\nEmail: "
-                                        .$email.
-                                        "\n------------------------------\n"
-                                        .$message.
-                                        "\n------------------------------\n\n");
-                }
-            }else{
-                echo "Please fill in the required information.<br>";
-            }
-        }else{
-            echo "please, submit the form.";
-        }
-        }
-    ?>
+    <div class="container">
+      <header>
+        <div class="logo">
+          <a href="index.php">
+            <img src="./resources/logo_english.png" alt="NHLStenden logo" width="100%">
+          </a>
+        </div>
+        <div class="headertext">
+          <p><a href="">Dance programm</a></p>
+          <p><a href="">Signup</a></p>
+          <p><a href="">Questions</a></p>
+          <p><a href="">Feedback</a></p>
+        </div>
+      </header>
+      <aside>
+        <img src="./resources/lineArt.png" alt="Lineart ballerina" width="100%">
+      </aside>
+      <main>
+        <div class="nested">
+          <div class = "content1">
+            <img src="./resources/hiphop.jpg" alt="hiphop dance" width="100%" height="100%">
+          </div>
+          <div class = "content2">
+            <img src="./resources/ballet.jfif" alt="ballet dance" width="100%" height="100%">
+          </div>
+          <div class = "content3">
+            <img src="./resources/flamenco.jfif" alt="flamenco dance" width="100%" height="100%">
+          </div>
+          <div class = "content4">
+            <img src="./resources/contemporary.jfif" alt="contemporary dance" width="100%" height="100%">
+          </div>
+        </div>
+      </main>
+      <info>
+        info
+      </info>
+      <announcements>
+        <div class="nestedAnnounce"><h2>Events</h2></div>
+        <div class="nestedAnnounce">Test</div>
+        <div class="nestedAnnounce">Test</div>
+        <div class="nestedAnnounce">Test</div>
+        <div class="nestedAnnounce">Test</div>
+      </announcements>
+      <footer>
+        footer
+      </footer>
+    </div>
   </body>
 </html>
