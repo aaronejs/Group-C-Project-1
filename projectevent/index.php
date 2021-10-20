fe59f49b7c95b0293f944cc6fb803635c1881572
Harapthewhite <91067834+Harapthewhite@users.noreply.github.com>
2021-10-20 15:15:19 +0200

    made projectevent code more efficient
    now there can be no error if someone inputs different number of days, months, years or text

--- projectevent/index.php
+++ projectevent/index.php
@@ -11,15 +11,30 @@
             //using the file function to split strings from the text file into arrays
             $lines=file("newfile.txt");
             //usign explode to further create arrays for each individual element
-            //$lines[0] is the number of events you want to add
-            for($index=0;$index<$lines[0];$index++)
-            {$day=explode(" ",$lines[1]);
-             $month=explode(" ",$lines[2]);
-             $year=explode(" ",$lines[3]);
-             $text=explode(";",$lines[4]);
+
+            $day=explode(" ",$lines[0]);
+            $month=explode(" ",$lines[1]);
+            $year=explode(" ",$lines[2]);
+            $text=explode(";",$lines[3]);
+
+            //creating an array to find the min number of count for each array
+            $events[0]=count($day);
+            $events[1]=count($month);
+            $events[2]=count($year);
+            $events[3]=count($text);
+
+            //checking the smallest number of elements from all arrays
+            //in order to not have errors from the different number of inputs for each element(array)
+            $min=$events[0];
+            for($index=1;$index<4;$index++)
+            {
+              if($events[$index]<$min)
+              {
+                $min=$events[$index];
+              }
             }
 
-            for($index=0;$index<$lines[0];$index++){
+            for($index=0;$index<$min;$index++){
             ?>
             <table class="padtop">
             <tr>
