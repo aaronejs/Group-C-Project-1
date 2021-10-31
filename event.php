d1930a6c5403b212286c1e28711ea2d982518171
Howtofail <dragos.munteanu@student.nhlstenden.com>
2021-10-31 22:24:29 +0100

    added comments to the event code

--- event.php
+++ event.php
@@ -26,9 +26,15 @@
             }
             //min is the minimum number of events
 
+            //made an array for the months and added a random first element
+            //in order for the months to match the array index
+            //for example now January is $array_Months[1] because it is the first month
             $months="a January February March April May June July August September October November December";
             $array_Months=explode(" ",$months);
+            //the $array_Months is used to display the month inputed in the "Events.txt" as string rather than a number
             for($index=0;$index<$min;$index++){
+              //this if checks if the input is either an event or an announcement
+              //if $date[$index]==0 the input is an announcement
               if($date[$index]==0){
                 ?>
                 <div class="nestedAnnounce">
@@ -36,6 +42,8 @@
               </div>
               <?php
               }
+              //this if checks if the length of the date entered is higher than 10
+              //if true, it means the event has a start and an end date
               else if(strlen($date[$index])>10){
                 $month_First=substr($date[$index],3,2);
                 $month_Second=substr($date[$index],14,2);
@@ -46,6 +54,7 @@
                 </div>
                 <?php
               }
+              //if false, it means the event only lasts for one day
               else{
                 $month_First=substr($date[$index],3,2)
                 ?>
