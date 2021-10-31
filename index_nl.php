fba3171e4247f4d4f939975c34a4c94710c086ee
Howtofail <dragos.munteanu@student.nhlstenden.com>
2021-10-31 22:32:39 +0100

    fixed events on index.nl page

--- index_nl.php
+++ index_nl.php
@@ -100,23 +100,6 @@
         </div>
       </div>
       <div class="announcements">
-        <div class="nestedAnnounce"><h2>Evenementen</h2></div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
-        <div class="nestedAnnounce">Test</div>
         <?php
         include 'event.php';
         event();
